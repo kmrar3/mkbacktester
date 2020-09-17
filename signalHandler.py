85ae75840a63c2b28348d0fb4d4c3bc007021775
kmrar3 <40686216+kmrar3@users.noreply.github.com>
2020-09-17 16:10:28 +1000

    Update signalHandler.py

--- signalHandler.py
+++ signalHandler.py
@@ -1,6 +1,7 @@
 class signalHandler:
 
     def __init__(self,stop_loss,take_profit,broker_cost,data):
+        # Parameters initialised
         self.original_stop_loss = stop_loss
         self.original_take_profit = take_profit
         self.broker_cost = broker_cost
@@ -23,17 +24,21 @@ class signalHandler:
 
     ############### Helpers ###############
 
-    # Floors or ceils PL 
     def bandPL(self,PL):
+        # Floors or ceils PL
+        # Check for take profit and stop loss value and return PL
         if PL > self.take_profit:
             PL = self.take_profit
         elif PL < self.stop_loss:
             PL = self.stop_loss
         return PL
 
-    # Only called when a trade happens
+    
     def closeTrade(self,PL):
+        # Only called when a trade happens
         # Reseting Current position,action AND toal profit
+        # Set the current action based on previous traded position
+        # Calculate total profit
         if self.prev_traded_position == -1:
             self.current_action = "short_buy"
         elif self.prev_traded_position == 1:
@@ -47,10 +52,9 @@ class signalHandler:
         self.stop_loss = self.original_stop_loss
         self.take_profit = self.original_take_profit
         
-    # Called every iteration to ADD stats into an array.
-    # These arrays will be later copied into a DATAFRAME
     def saveStats(self,PL,index):
-                
+        # Called every iteration to ADD stats into an array.        
+        # These arrays will be later copied into a DATAFRAME
         if self.prev_traded_position == 1:
             self.action[index] = self.current_action
         
@@ -63,9 +67,11 @@ class signalHandler:
         self.arr_PL[index] = PL
         self.arr_total_profit[index] = self.total_profit
 
-    # Transfers all the data into the DATAFRAME
-    # The constructor is initialised with
+    
+    
     def getData(self):
+        # Transfers all the data into the DATAFRAME
+        # The constructor is initialised with
         self.data['action'] = self.action
         self.data['P/L'] = self.arr_PL
         self.data['Total profit'] = self.arr_total_profit
@@ -73,7 +79,10 @@ class signalHandler:
 
     ############### Actions ###############
     def buy(self,current_price,index):
-        
+        # PL value initialised to 0, current action set to 'buy'
+        # previous traded position is set along with prev traded price
+        # Data is saved to later copied into dataframe
+        # total profit is returned
         PL = 0 # <----- Default for if currently holding 
         if self.prev_traded_position == 0:
             self.current_action = "buy"
@@ -89,8 +98,12 @@ class signalHandler:
             #     self.take_profit *= 1.02 # <----- EDIT Based on how you wish to scale take_profit
             #     self.stop_loss *= 2
             self.checkStopConditions(current_price,index)
-
+        
         elif self.prev_traded_position == -1:
+            # if previous position was 'Sell' then set current action to 'short buy'
+            # Calculate PL value
+            # Take profit or stop loss value is fetched depending on PL
+            # Trade closed and data saved
             self.current_action = "short_buy"
             PL = (self.prev_traded_position*(current_price - self.prev_traded_price))-self.broker_cost
             PL = self.bandPL(PL)
@@ -103,7 +116,10 @@ class signalHandler:
         return self.total_profit
 
     def sell(self,current_price,index):
-        
+        # Current action is set based on previous traded position (0->'Short', 1->'Sell', 0->'Hold')
+        # previous traded position reset
+        # Data saved for later copying into dataframe
+        # total profit value returned
         PL = 0 # <----- Default for if currently holding
         if self.prev_traded_position == 0:
             self.current_action = "short"
@@ -128,10 +144,14 @@ class signalHandler:
 
         return self.total_profit
     
-    # Called from MAIN code when the HOLD signal is received
-    # ALSO called by the BUY AND SELL functions when
-    # the broker sequentially receives the same signal.
+    
     def checkStopConditions(self,current_price,index):
+        # Called from MAIN code when the HOLD signal is received
+        # ALSO called by the BUY AND SELL functions when
+        # the broker sequentially receives the same signal.
+        # current action is set as 'hold'
+        # PL value calculated and trade closed
+        # total profit returned
         PL = 0
         self.current_action = "hold"
         
